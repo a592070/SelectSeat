4293d3600328ff4e207dbc75d725bf01e3b8ffaa
a592070 <a592070@gmail.com>
2021-02-01 18:33:43 +0800

    2021 0201

--- grails-app/domain/selectseat/Seat.groovy
+++ grails-app/domain/selectseat/Seat.groovy
@@ -1,72 +1,17 @@
 package selectseat
 
-import grails.plugins.redis.RedisService
-import utility.ToolService
-
 class Seat {
-/* Default (injected) attributes of GORM */
     Long id
     Long version
 
-    // [0,0,0,0,0]
-    List<Integer> seatBitmap
+    int status
 
-    String columnName
-    int rowAmount
-    String seatCode
-
-    static belongsTo = [zone:Zone]
-
-    static final String CODE_PREFIX = "S"
-
-    transient RedisService redisService
-    transient String REDIS_KEY_SEAT_MAP
+    static belongsTo = [seatMap: SeatMap]
 
     static constraints = {
-        columnName nullable: false
-        rowAmount nullable: false
-        seatCode unique: true
     }
-
-//    def getSeatBitMap(){
-//        redisService.set(REDIS_KEY_SEAT_MAP, seatBitmap)
-//    }
 
     def beforeInsert(){
-        println("====Seat beforeInsert====")
-        def seatList = []
-        for (i in 0..<rowAmount) {
-            seatList << 0
-        }
-        seatBitmap = seatList
-
-//        def tmpNo = CODE_PREFIX + zoneId + columnName + ToolService.generateRandomWord(5,true)
-//        while(Seat.countBySeatCode(tmpNo) ){
-//            tmpNo = CODE_PREFIX + zoneId + columnName +ToolService.generateRandomWord(5,true)
-//        }
-//        seatCode = tmpNo
-
-        seatCode = zone.zoneCode + CODE_PREFIX + columnName
+        status = 0
     }
-    def afterInsert(){
-        updateRedisKeySeatMap()
-    }
-    def afterUpdate(){
-        updateRedisKeySeatMap()
-    }
-    def onLoad() {
-        println "before load"
-//        updateRedisKeySeatMap()
-    }
-    def afterLoad(){
-        println "after load"
-        updateRedisKeySeatMap()
-    }
-    def updateRedisKeySeatMap(){
-        REDIS_KEY_SEAT_MAP = seatCode
-    }
-
-
-
-
 }
